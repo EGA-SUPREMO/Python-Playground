1d9ae6b27643ecd6793f89c6191f6e024d153fa5
EGA-SUPREMO <cxiesestro@gmail.com>
2022-08-20 13:11:45 -0400

    finish sending spam python project

--- send spam/files.py
+++ send spam/files.py
@@ -1,44 +1,4 @@
 import json
-import objcrypt
-
-
-def writeEncJSON(key):
-    data = {}
-
-    with open('config.cg', 'w') as outfile:
-        crypter = objcrypt.Crypter(key, 'cbc')
-        json_file = json.dumps(data, ensure_ascii=False)
-        enc_json = crypter.encrypt_json(json_file)
-        outfile.write(enc_json)
-        outfile.close()
-
-
-def writeJSON():
-    data = {}
-    data['config'] = []
-    data['config'].append({
-        'email': '@gmail.com',
-        'password': '',
-        'keywords': ['mother', 'worried'],
-        'white_list': ['@gmail.com'],
-        'data_emails': ['', '', ''],
-        'websites': ''
-    })
-
-    with open('temp.tm', 'w') as outfile:
-        json_file = json.dumps(data, ensure_ascii=False)
-        outfile.write(json_file)
-        outfile.close()
-
-
-def readEncJSON(key):
-    with open('config.cg', 'r') as json_file:
-        crypter = objcrypt.Crypter(key, 'cbc')
-        str_json = json_file.read()
-        data = crypter.decrypt_json(str_json)
-        dec_json = json.loads(data)
-        return dec_json['config']
-
 
 def readJSON():
     with open('temp.tm', 'r') as json_file:
--- send spam/send_spam.py
+++ send spam/send_spam.py
@@ -1,12 +1,46 @@
 import yagmail
+import files
 
+import time
+import random
 
+try:
+    data = files.readJSON()[0]
+except Exception:
+    data = files.readJSON()[0]
 
-yag = yagmail.SMTP(email, passw)
+#yag = yagmail.SMTP(data["email"], data["apppassword"])
 
-def sendEmail(email, passw, receiver, esubject, body):
+names = open("names.tm" , "r", encoding="utf8");
+emails = open("emails.tm" , "r", encoding="utf8");
+
+names = names.read().split("\n")
+emails = emails.read().split("\n")
+
+def sendEmail(receiver, esubject, body):
     yag.send(
         to=receiver,
         subject=esubject,
         contents=body
     )
+
+    time.sleep(random.randint(120, 300))
+
+x = 0
+
+def send_spam(j, firstTime):
+    for i in range(j, j + 10):
+        gretting = "Hello,\n\n"
+        if names[i] != "":
+            gretting = "Hello " + names[i] + ",\n\n"
+        
+        body = gretting + data["data_emails"][firstTime]
+
+        sendEmail(emails[i], "Offer to add subtitles to your videos", body)
+
+    print("Ultima vez mandado para el id: "+ str(j+10))
+    print("CAMBIA LA X A: "+ str(x+10))
+
+send_spam(x, 0)
+if x > 9:
+    send_spam(x-10, 1)
